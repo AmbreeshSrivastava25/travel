3bf4a22ea8c8491344a8ede3b217282a1ca5a6d1
Ambreesh <ambreeshsri18@gmail.com>
2023-07-10 16:18:13 +0530

    solved issue of photo and added more handlebars

--- routes/blog.js
+++ routes/blog.js
@@ -3,11 +3,13 @@ const router=express.Router();
 const path=require("path");
 
 router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../views/index.html'));
+    res.render('home')
+    // res.sendFile(path.join(__dirname,'../views/index.html'));
 })
 
 router.get('/projects',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../views/projects.html'));
+    // res.sendFile(path.join(__dirname,'../views/projects.html'));
+    res.render('projects')
 })
 
 module.exports=router;
