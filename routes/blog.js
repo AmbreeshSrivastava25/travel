6c35c2c6a870120a5c6ef1497a7a2b112d2baa75
Ambreesh <ambreeshsri18@gmail.com>
2023-07-11 00:04:56 +0530

    Integrated firebase login

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,11 @@ const router=express.Router();
 const path=require("path");
 
 router.get('/',(req,res)=>{
+    //res.render('home')
+    res.sendFile(path.join(__dirname,'../views/index.html'));
+})
+
+router.get('/home',(req,res)=>{
     res.render('home')
     // res.sendFile(path.join(__dirname,'../views/index.html'));
 })
